ef618a2549f8447e23ce185fc529237bf67db65b
Samalou <137214216+Samalou@users.noreply.github.com>
2024-10-14 15:22:01 +0200

    ajout recherche

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -5,36 +5,64 @@ namespace App\Controller;
 use App\Factory\TrackFactory;
 use App\Service\AuthSpotifyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SearchType as SearchInputType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request; // Ajout de l'import correct pour Request
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TrackController extends AbstractController
 {
-
     private string $token;
 
-
-    public function __construct(private readonly AuthSpotifyService  $authSpotifyService,
-                                private readonly HttpClientInterface $httpClient,
-                                private readonly TrackFactory        $trackFactory
-    )
-    {
+    public function __construct(
+        private readonly AuthSpotifyService $authSpotifyService,
+        private readonly HttpClientInterface $httpClient,
+        private readonly TrackFactory $trackFactory
+    ) {
         $this->token = $this->authSpotifyService->auth();
     }
 
     #[Route('/track', name: 'app_track_index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/search?query=juicewrld&type=track&locale=fr-FR', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->token,
-            ],
-        ]);
+        $form = $this->createFormBuilder()
+            ->add('query', SearchInputType::class, [
+                'attr' => ['placeholder' => 'Rechercher'],
+                'label' => false,
+                'required' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Rechercher',
+                'attr' => ['class' => 'submit'],
+            ])
+            ->getForm();
 
-        $tracks = $this->trackFactory->createMultipleFromSpotifyData($response->toArray()['tracks']['items']);
+        $form->handleRequest($request);
+
+        $tracks = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $query = $form->get('query')->getData();
+
+            $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/search', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token,
+                ],
+                'query' => [
+                    'q' => $query,
+                    'type' => 'track',
+                    'locale' => 'fr-FR',
+                ],
+            ]);
+
+            $tracks = $this->trackFactory->createMultipleFromSpotifyData($response->toArray()['tracks']['items']);
+        }
 
         return $this->render('track/index.html.twig', [
+            'form' => $form->createView(),
             'tracks' => $tracks,
         ]);
     }
