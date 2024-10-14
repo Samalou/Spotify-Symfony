35941fd43afb2a7c2b9ca3efb0faffc93594ba38
Samalou <137214216+Samalou@users.noreply.github.com>
2024-10-14 11:09:37 +0200

    initial commit

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -17,7 +17,7 @@ class TrackController extends AbstractController
 
     public function __construct(private readonly AuthSpotifyService  $authSpotifyService,
                                 private readonly HttpClientInterface $httpClient,
-//                                private readonly TrackFactory         $trackFactory
+                                private readonly TrackFactory        $trackFactory
     )
     {
         $this->token = $this->authSpotifyService->auth();
@@ -26,14 +26,13 @@ class TrackController extends AbstractController
     #[Route('/track', name: 'app_track_index')]
     public function index(): Response
     {
-        // Make the GET request to the Spotify API with kazzey as the query and the token as the Authorization header
         $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/search?query=kazzey&type=track&locale=fr-FR', [
             'headers' => [
                 'Authorization' => 'Bearer ' . $this->token,
             ],
         ]);
-// Examples of how you could do this
-//        $tracks = $this->trackFactory->createMultipleFromSpotifyData($response->toArray()['tracks']['items']);
+
+        $tracks = $this->trackFactory->createMultipleFromSpotifyData($response->toArray()['tracks']['items']);
 
         return $this->render('track/index.html.twig', [
             'tracks' => $tracks,
---
+++ src/Factory/TrackFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Track;
+
+class TrackFactory
+{
+    public function createFromSpotifyData(array $spotifyData): Track
+    {
+        return new Track(
+            $spotifyData['disc_number'],
+            $spotifyData['duration_ms'],
+            $spotifyData['explicit'],
+            $spotifyData['external_ids']['isrc'] ?? '',
+            $spotifyData['external_urls']['spotify'],
+            $spotifyData['href'],
+            $spotifyData['id'],
+            $spotifyData['is_local'],
+            $spotifyData['name'],
+            $spotifyData['popularity'],
+            $spotifyData['preview_url'] ?? null,
+            $spotifyData['track_number'],
+            $spotifyData['type'],
+            $spotifyData['uri'],
+            $spotifyData['album']['images'][0]['url'] ?? null
+        );
+    }
+
+    public function createMultipleFromSpotifyData(array $spotifyTracksData): array
+    {
+        $tracks = [];
+        foreach ($spotifyTracksData as $spotifyTrackData) {
+            $tracks[] = $this->createFromSpotifyData($spotifyTrackData);
+        }
+        return $tracks;
+    }
+}
